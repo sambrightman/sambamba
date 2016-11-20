2ee0cd9f9cc7aae74b352c18811105f7c29c386e
Sam Brightman <sam.brightman@gmail.com>
2016-11-20 10:14:09 +0000

    Make version info generation Python 2.6 compatible

--- gen_ldc_version_info.py
+++ gen_ldc_version_info.py
@@ -7,8 +7,9 @@ if len(sys.argv) < 2:
     print("Usage: {} <path to ldmd2 executable>".format(sys.argv[0]))
     sys.exit(1)
 
-ldc = sys.argv[1].replace("ldmd2", "ldc2")
-ldc_output = subprocess.check_output([ldc, "-version"])
+ldc_command = sys.argv[1].replace("ldmd2", "ldc2")
+ldc = subprocess.Popen([ldc_command, "-version"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+ldc_output, _ = ldc.communicate()
 version_re = r"""^.+\((?P<LDC>[^\)]+)\):\n\s*based on DMD (?P<DMD>\S+) and LLVM (?P<LLVM>\S+)\n(?:\s*built with (?P<BOOTSTRAP>.*)\n)?"""
 match = re.match(version_re, ldc_output, re.MULTILINE)
 
@@ -19,4 +20,4 @@ print("module utils.ldc_version_info_;")
 for component, version in match.groupdict().items():
     if version is None:
         version = "version not available"
-    print("immutable {}_VERSION_STRING = \"{}\";".format(component, version))
+    print("immutable {0}_VERSION_STRING = \"{1}\";".format(component, version))
